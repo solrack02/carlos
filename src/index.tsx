e71b5b8d3eee9d76a3c63a9a4b42826c475a954e
Carlos A. <carlosvsk@gmail.com>
2024-11-12 11:09:52 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -1486,7 +1486,7 @@ stls.justifyContent({ pass: { arrayValue: ['center'] }}), stls.alignItems({ pass
 
       largura: "16px",
 
-      preenchimento: ['transparent'],
+      preenchimento: ['black'],
 
       args,
     }}/>],
@@ -1611,7 +1611,7 @@ stls.justifyContent({ pass: { arrayValue: ['center'] }}), stls.alignItems({ pass
 
       largura: "16px",
 
-      preenchimento: ['transparent'],
+      preenchimento: ['black'],
 
       args,
     }}/>],
@@ -2053,37 +2053,7 @@ stls.justifyContent({ pass: { arrayValue: ['center'] }}), stls.alignItems({ pass
 
             expectedVal:[ ],
 
-            childrenItems:[(...args:any) => <Elements.SvgView1 pass={{
-      componentSvg: (Svg:any, SvgObj:any) => {
-        const { Defs, Stop, Path, LinearGradient, G, Circle, Rect, Mask } = SvgObj;
-        return (props:any) => (<Svg
-              xmlns="http://www.w3.org/2000/svg"
-              width={20}
-              height={14}
-              viewBox="0 0 20 14"
-              {...props}
-              >
-              <Path
-              fillRule="evenodd"
-              d="M7.707.293a1 1 0 0 1 0 1.414L3.414 6H19a1 1 0 1 1 0 2H3.414l4.293 4.293a1 1 0 1 1-1.414 1.414l-6-6a1 1 0 0 1 0-1.414l6-6a1 1 0 0 1 1.414 0Z"
-              clipRule="evenodd"
-              {...props}
-              />
-              </Svg>)
-      },
-
-      svgOriginal: `
-        <svg></svg>
-      `,
-
-      altura: "30px",
-
-      largura: "30px",
-
-      preenchimento: ['transparent'],
-
-      args,
-    }}/>],
+            childrenItems:[() =><></>],
 
             args,
           }}/>
