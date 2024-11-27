6672dad2ff94521c4d6def8ddd83b4339a8fea9d
Carlos A. <carlosvsk@gmail.com>
2024-11-27 11:29:15 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -1988,7 +1988,6 @@ stls.width({ pass: { arrayValue: [jsvals.varReader({pass: {
 
           args,
         }}/>, 
-        
 
         (...args: any) => <Elements.Screen3 pass={{
           pathScreen:"a2",
@@ -2430,39 +2429,6 @@ stls.justifyContent({ pass: { arrayValue: ['center'] }}), stls.alignItems({ pass
             args,
           }}/>
         ],
-
-          startFunctions:[()=>{}],
-
-          args,
-        }}/>, 
-
-        (...args: any) => <Elements.Screen3 pass={{
-          pathScreen:"smallbook",
-
-          styles:[
-              {
-                backgroundColor: '#101',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100%',
-                width: '100%',
-              }
-              ],
-
-          screenElements:[() => {
-                  const textStyle = {
-                    fontSize: 20,
-                    color: '#fff2',
-                    textAlign:'center',
-                    maxWidth: 200,
-                    // maxWidth: '200px'<= #ATTENTION: Native ERROR! No string!
-                  };
-
-                  return (
-                    <RN.Text style={textStyle}>
-                      {'Adicione Elementos nessa tela!'}
-                    </RN.Text>);
-                }],
 
           startFunctions:[()=>{}],
 
